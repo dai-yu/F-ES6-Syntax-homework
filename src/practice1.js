63843ed929b854ea2280ffd79c72e31ee91191d6
dai-yu <1435480344@qq.com>
2020-11-10 22:45:13 +0800

    finish practice2

--- src/practice1.js
+++ src/practice1.js
@@ -1,2 +1,10 @@
-const parseData
+const parseData = (input) => {
+  let result = [{}, {}];
+  input.data.map((dataItem, i) => {
+    input.column.map((columnItem, index) => {
+      result[i][columnItem.name] = dataItem[index];
+    });
+  });
+  return result;
+};
 export { parseData };
--- src/practice2.js
+++ src/practice2.js
@@ -1,2 +1,9 @@
-const inject
+const inject = (items, sections) => {
+  let newItems = [...items];
+  sections.map((obj) => {
+    let oiginalItem = items[obj.index];
+    newItems.splice(newItems.indexOf(oiginalItem), 0, obj.content);
+  });
+  return newItems;
+};
 export { inject };
